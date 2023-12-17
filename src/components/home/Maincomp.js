4bab7f009c838f0fc74361047fa3ee9f70856620
Ambika <2304ambika.gupta@gmail.com>
2023-12-17 18:03:12 +0530

    init commit

---
+++ src/components/home/Maincomp.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Banner from './Banner';
+import './home.css';
+
+const Maincomp = () => {
+  return (
+    <div className='home_sectiom'>
+        <div className="banner_part">
+            <Banner />
+        </div>
+   
+    </div>
+  )
+}
+
+export default Maincomp
