40be08de58727089938602f2eaaf75df6fe7c71b
Anushka <anushka.garg1812@gmail.com>
2023-12-23 17:06:45 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import './App.css';
 import Navbaar from './components/header/Navbaar';
 import Newnav from './components/newnavbaar/Newnav';
 import Maincomp from './components/home/Maincomp';
+import Slides from './components/home/Slide';
+import Footer from './components/footer/footer';
 import SignUp from './components/signup_signin/SignUp';
 import Sign_in from './components/signup_signin/Sign_in';
 import { Routes,Route } from 'react-router-dom';
-
 function App() {
   return (
     <>
@@ -16,6 +17,8 @@ function App() {
       <Route path='/login' element={<Sign_in />}  />
       <Route path='/register' element={<SignUp />}  />
     </Routes>
+    
+    <Footer/>
     
     </>
   );
